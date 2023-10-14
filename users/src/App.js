c7b0898c3059277e79e6117ad806d641dda81953
ar6aaz ar6aaz <ar6aaz@protonmail.com>
2023-10-14 18:14:03 +0000

    created simple app

---
+++ users/src/App.js
@@ -0,0 +1,19 @@
+import UserForm from "./UserForm";
+import UserList from "./UserList"
+import {useState} from 'react';
+
+function App() {
+  const [users,setUsers] = useState([]);
+
+  const onUserAdd = (user) => {
+    setUsers([...users, user])
+  }
+  return <div>
+      <UserForm onUserAdd={onUserAdd}/>
+      <br />
+      <hr />
+      <UserList users={users} />
+  </div>
+}
+
+export default App;
