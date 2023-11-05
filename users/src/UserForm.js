79d5776426528fb487e1cf02d3a9de1df5fd9b8e
ar6aaz ar6aaz <ar6aaz@protonmail.com>
2023-11-05 12:09:07 +0000

    added tests for recording function calls

--- users/src/UserForm.js
+++ users/src/UserForm.js
@@ -1,28 +1,28 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
-function UserForm({ onUserAdd }){
-    const [email, setEmail] = useState('');
-    const [name, setName] = useState('');
+function UserForm({ onUserAdd }) {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        onUserAdd({ name, email });
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-    
-    return (<form onSubmit={handleSubmit}>
-            <div>
-                <label>Name</label>
-                <input value={name} onChange = {(e) => setName(e.target.value)}/>
-            </div>
-            <div>
-                <label>Email</label>
-                <input value={email} onChange = {(e) => setEmail(e.target.value)}/>
-            </div>
-            <div>
-                <button>Add user</button>
-            </div>
-        </form>)
-    }
-    
-    export default UserForm;
+    onUserAdd({ name, email });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Name</label>
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+      </div>
+      <div>
+        <label>Email</label>
+        <input value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <button>Add User</button>
+    </form>
+  );
+}
+
+export default UserForm;
--- users/src/UserList.js
+++ users/src/UserList.js
@@ -1,25 +1,48 @@
-function UserList({users}) {
+import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import UserForm from './UserForm';
 
-    const renderedUsers = users.map((user) => {
-        return (
-        <tr key={user.name}>
-            <td>{user.name}</td>
-            <td>{user.email}</td>
-        </tr>
-        )
-    });
+test('it shows two inputs and a button', () => {
+  // render the component
+  render(<UserForm />);
 
-    return (<table>
-            <thead>
-                <tr>
-                    <th>Name </th>
-                    <th>Email </th>
-                </tr>    
-            </thead>
-            <tbody>
-                {renderedUsers}
-            </tbody>        
-        </table>)
-}
+  // Manipulate the component or find an element in it
+  const inputs = screen.getAllByRole('textbox');
+  const button = screen.getByRole('button');
 
-export default UserList;
+  // Assertion - make sure the component is doing
+  // what we expect it to do
+  expect(inputs).toHaveLength(2);
+  expect(button).toBeInTheDocument();
+});
+
+test('it calls onUserAdd when the form is submitted', () => {
+  // NOT THE BEST IMPLEMENTATION
+  const argList = [];
+  const callback = (...args) => {
+    argList.push(args);
+  };
+  // Try to render my component
+  render(<UserForm onUserAdd={callback} />);
+
+  // Find the two inputs
+  const [nameInput, emailInput] = screen.getAllByRole('textbox');
+
+  // Simulate typing in a name
+  user.click(nameInput);
+  user.keyboard('jane');
+
+  // Simulate typing in an email
+  user.click(emailInput);
+  user.keyboard('jane@jane.com');
+
+  // Find the button
+  const button = screen.getByRole('button');
+
+  // Simulate clicking the button
+  user.click(button);
+
+  // Assertion to make sure 'onUserAdd' gets called with email/name
+  expect(argList).toHaveLength(1);
+  expect(argList[0][0]).toEqual({ name: 'jane', email: 'jane@jane.com' });
+});
